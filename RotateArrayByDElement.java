159bc55ad063755c31de1680b50b23ead4526b9f
RameshwaramSingh <156481890+RameshwaramSingh@users.noreply.github.com>
2025-04-10 19:20:50 +0530

    Create RotateArrayByDElement.java

---
+++ RotateArrayByDElement.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class RotateArrayByDElement {
+    static void rotateArr(int arr[],int d){
+        int n = arr.length;
+        d = d%n; //handle the case where d>n
+        int start =0, end = d-1;
+        while(start<=end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        start = d;
+        end = arr.length -1;
+        while(start<=end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        start =0;
+        end = arr.length-1;
+        while(start<=end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int d =2;
+        rotateArr(arr,d);
+    }
+}
