3b16a392e420302704b9ca548ed1c4eee620a825
Rameshwaram Singh <rameshwaramsingh3@gmail.com>
2025-06-06 23:46:03 +0530

    Create H-Index.java

---
+++ array/H-Index.java
@@ -0,0 +1,16 @@
+
+https://leetcode.com/problems/h-index/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+        return 0;
+    }
+}
