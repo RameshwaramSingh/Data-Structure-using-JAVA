2ad5aa623d10cf4f7027d0935dc09c6143579634
Rameshwaram Singh <rameshwaramsingh3@gmail.com>
2025-06-08 18:52:16 +0530

    Create SwapNodesInPairs.java

---
+++ LinkedList/SwapNodesInPairs.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+https://leetcode.com/problems/swap-nodes-in-pairs/description/?envType=problem-list-v2&envId=linked-list
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        while(head!=null && head.next!=null){
+
+            //Node to be swapped
+            ListNode first = head;
+            ListNode second = head.next;
+
+            //Swapping
+            prev.next = second;
+            first.next = second.next;
+            second.next = first;
+            
+            // re positioning pointer 
+            prev = first;
+            head = first.next;
+        }
+        return dummy.next;
+    }
+}    
