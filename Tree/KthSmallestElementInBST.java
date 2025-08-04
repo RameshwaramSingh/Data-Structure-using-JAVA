ca2412f9472bc04c4919f0ec211dd57a70c8e220
Rameshwaram Singh <rameshwaramsingh3@gmail.com>
2025-08-04 20:35:14 +0530

    Create KthSmallestElementInBST.java

---
+++ Tree/KthSmallestElementInBST.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//leetcode ques no 230
+https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+
+class Solution {
+
+    // Inorder traversal is used
+    
+    int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        return helper(root,k).val;
+    }
+    public TreeNode helper(TreeNode root, int k){
+        if (root == null){
+            return null;
+        }
+        TreeNode left = helper(root.left,k);
+        if(left != null){
+            return left;
+        }
+        count++;
+        if(count == k){
+            return root;
+        }
+        return helper(root.right,k);
+    }
+}
